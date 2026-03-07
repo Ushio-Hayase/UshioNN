475e9e5184b9a3c85a2bbfcfe6b866938e3c48cb
UshioHayase <UshioHayase@gmail.com>
2026-03-08 00:27:08 +0900

    feat: 텐서 클래스 수정

--- src/core/tensor.cpp
+++ src/core/tensor.cpp
@@ -114,7 +114,8 @@ Tensor::Tensor(const std::vector<uint64_t>& shape, const T* ptr)
     cpu_data_ptr_.reset(std::malloc(total_bytes_));
 #endif
 
-    std::copy(ptr, ptr + (total_bytes_ / sizeof(T)), (T*)cpu_data_ptr_.get());
+    std::copy(ptr, ptr + (total_bytes_ / sizeof(T)),
+              static_cast<T*>(cpu_data_ptr_.get()));
 }
 
 Tensor::Tensor(const std::vector<uint64_t>& shape, void* gpu_ptr, DType type)
@@ -292,7 +293,358 @@ Tensor& Tensor::operator+=(const Tensor& other)
             float* this_cpu_ptr = static_cast<float*>(cpu_data_ptr_.get());
             float* other_cpu_ptr =
                 static_cast<float*>(other.cpu_data_ptr_.get());
-            size_t total_elements = total_bytes_ / 4;
+            size_t total_elements = total_bytes_ / sizeof(float);
+
+            size_t align_step = 1;
+#if SIMD_LEVEL == 4
+            align_step = 16;
+#elif SIMD_LEVEL == 3 || SIMD_LEVEL == 2
+            align_step = 8;
+#elif SIMD_LEVEL == 1
+            align_step = 4;
+#endif
+
+            if (total_elements < MULTI_THREAD_BASELINE)
+                number_of_thread = 1;
+
+            size_t chunk_size = total_elements / number_of_thread;
+            chunk_size &= ~(align_step - 1);
+
+            auto worker = [](float* src, float* tgt, size_t start, size_t end) {
+#if SIMD_LEVEL == 4
+                size_t i = start;
+                size_t limit = end - ((end - start) & ~15ULL);
+                for (; i < limit; i += 16)
+                {
+                    __m512 data_origin(_mm512_load_ps(src + i));
+                    __m512 data_other(_mm512_load_ps(tgt + i));
+                    _mm512_store_ps(src + i,
+                                    _mm512_add_ps(data_origin, data_other));
+                }
+
+                for (; i < end; ++i)
+                {
+                    src[i] += tgt[i];
+                }
+
+#elif SIMD_LEVEL == 3 || SIMD_LEVEL == 2
+                size_t i = start;
+                size_t limit = end - ((end - start) & ~7ULL);
+                for (; i < limit; i += 8)
+                {
+                    __m256 data_origin(_mm256_load_ps(src + i));
+                    __m256 data_other(_mm256_load_ps(tgt + i));
+                    _mm256_store_ps(src + i,
+                                    _mm256_add_ps(data_origin, data_other));
+                }
+
+                for (; i < end; ++i)
+                {
+                    src[i] += tgt[i];
+                }
+
+#elif SIMD_LEVEL == 1
+                size_t i = start;
+                size_t limit = end - ((end - start) & ~3ULL);
+                for (; i < limit; i += 4)
+                {
+                    __m128 data_origin(_mm_load_ps(src + i));
+                    __m128 data_other(_mm_load_ps(tgt + i));
+                    _mm_store_ps(src + i, _mm_add_ps(data_origin, data_other));
+                }
+
+                for (; i < end; ++i)
+                {
+                    src[i] += tgt[i];
+                }
+
+#elif SIMD_LEVEL == 0
+                for (size_t i = start; i < end; ++i)
+                    src[i] += tgt[i];
+#endif
+            };
+
+            std::vector<std::thread> threads;
+            if (number_of_thread <= 1)
+            {
+                worker(this_cpu_ptr, other_cpu_ptr, 0, total_elements);
+                break;
+            }
+            threads.reserve(number_of_thread);
+            size_t current_start = 0;
+
+            for (size_t i = 0; i < number_of_thread - 1; ++i)
+            {
+                size_t current_end = current_start + chunk_size;
+                if (current_end > total_elements)
+                    current_end = total_elements;
+                threads.emplace_back(worker, this_cpu_ptr, other_cpu_ptr,
+                                     current_start, current_end);
+                current_start = current_end;
+            }
+
+            threads.emplace_back(worker, this_cpu_ptr, other_cpu_ptr,
+                                 current_start, total_elements);
+            for (auto& t : threads)
+                t.join();
+
+            break;
+        }
+        case DType::FP16: {
+            fp16_t* this_cpu_ptr = static_cast<fp16_t*>(cpu_data_ptr_.get());
+            fp16_t* other_cpu_ptr =
+                static_cast<fp16_t*>(other.cpu_data_ptr_.get());
+            for (int i = 0; i < total_bytes_ / elementSize(); ++i)
+                *(this_cpu_ptr + i) += *(other_cpu_ptr + i);
+            break;
+        }
+        case DType::BF16: {
+            bf16_t* this_cpu_ptr = static_cast<bf16_t*>(cpu_data_ptr_.get());
+            bf16_t* other_cpu_ptr =
+                static_cast<bf16_t*>(other.cpu_data_ptr_.get());
+
+            break;
+        }
+        case DType::FP8_e5m2: {
+            break;
+        }
+        case DType::FP8_e4m3: {
+            break;
+        }
+        case DType::FP4: {
+            break;
+        }
+        }
+    }
+    else if (location_ == DataLocation::DEVICE)
+    {
+        addAssignGpu(other, type_);
+    }
+
+    return *this;
+}
+
+Tensor& Tensor::operator*=(const Tensor& other)
+{
+    ASSERT_MESSAGE(this->location_ != DataLocation::NONE,
+                   "Tensor not assigned.");
+    ASSERT_MESSAGE(this->type_ != other.type_,
+                   "The two tensors are of different types.");
+    ASSERT_MESSAGE(this->shape_ != other.shape_,
+                   "Two tensors have different sizes.");
+    ASSERT_MESSAGE(this->location_ != other.location_,
+                   "Both tensors must be in the same position.");
+
+    if (this->location_ == DataLocation::HOST)
+    {
+        uint32_t number_of_thread = std::thread::hardware_concurrency();
+
+        if (number_of_thread == 0)
+            number_of_thread = 1;
+
+        switch (type_)
+        {
+        case DType::FP64: {
+            double* this_cpu_ptr = static_cast<double*>(cpu_data_ptr_.get());
+            double* other_cpu_ptr =
+                static_cast<double*>(other.cpu_data_ptr_.get());
+            size_t total_elements = total_bytes_ / 8;
+
+            size_t align_step = 1;
+#if SIMD_LEVEL == 4
+            align_step = 8;
+#elif SIMD_LEVEL == 3 || SIMD_LEVEL == 2
+            align_step = 4;
+#elif SIMD_LEVEL == 1
+            align_step = 2;
+#endif
+
+            if (total_elements < MULTI_THREAD_BASELINE)
+                number_of_thread = 1;
+
+            size_t chunk_size = total_elements / number_of_thread;
+            chunk_size &= ~(align_step - 1);
+
+            auto worker = [](double* src, double* tgt, size_t start,
+                             size_t end) {
+#if SIMD_LEVEL == 4
+                size_t i = start;
+                size_t limit = end - ((end - start) & ~7ULL);
+                for (; i < limit; i += 8)
+                {
+                    __m512d data_origin(_mm512_load_pd(src + i));
+                    __m512d data_other(_mm512_load_pd(tgt + i));
+                    _mm512_store_pd(src + i,
+                                    _mm512_mul_pd(data_origin, data_other));
+                }
+
+                for (; i < end; ++i)
+                {
+                    src[i] *= tgt[i];
+                }
+
+#elif SIMD_LEVEL == 3 || SIMD_LEVEL == 2
+                size_t i = start;
+                size_t limit = end - ((end - start) & ~3ULL);
+                for (; i < limit; i += 4)
+                {
+                    __m256d data_origin(_mm256_load_pd(src + i));
+                    __m256d data_other(_mm256_load_pd(tgt + i));
+                    _mm256_store_pd(src + i,
+                                    _mm256_mul_pd(data_origin, data_other));
+                }
+
+                for (; i < end; ++i)
+                {
+                    src[i] *= tgt[i];
+                }
+
+#elif SIMD_LEVEL == 1
+                size_t i = start;
+                size_t limit = end - ((end - start) & ~1ULL);
+                for (; i < limit; i += 2)
+                {
+                    __m128d data_origin(_mm_load_pd(src + i));
+                    __m128d data_other(_mm_load_pd(tgt + i));
+                    _mm_store_pd(src + i, _mm_mul_pd(data_origin, data_other));
+                }
+
+                for (; i < end; ++i)
+                {
+                    src[i] *= tgt[i];
+                }
+
+#elif SIMD_LEVEL == 0
+                for (size_t i = start; i < end; ++i)
+                    src[i] *= tgt[i];
+#endif
+            };
+
+            std::vector<std::thread> threads;
+            if (number_of_thread <= 1)
+            {
+                worker(this_cpu_ptr, other_cpu_ptr, 0, total_elements);
+                break;
+            }
+            threads.reserve(number_of_thread);
+            size_t current_start = 0;
+
+            for (size_t i = 0; i < number_of_thread - 1; ++i)
+            {
+                size_t current_end = current_start + chunk_size;
+                if (current_end > total_elements)
+                    current_end = total_elements;
+                threads.emplace_back(worker, this_cpu_ptr, other_cpu_ptr,
+                                     current_start, current_end);
+                current_start = current_end;
+            }
+
+            threads.emplace_back(worker, this_cpu_ptr, other_cpu_ptr,
+                                 current_start, total_elements);
+            for (auto& t : threads)
+                t.join();
+
+            break;
+        }
+        case DType::FP32: {
+            float* this_cpu_ptr = static_cast<float*>(cpu_data_ptr_.get());
+            float* other_cpu_ptr =
+                static_cast<float*>(other.cpu_data_ptr_.get());
+            size_t total_elements = total_bytes_ / 8;
+
+            size_t align_step = 1;
+#if SIMD_LEVEL == 4
+            align_step = 16;
+#elif SIMD_LEVEL == 3 || SIMD_LEVEL == 2
+            align_step = 8;
+#elif SIMD_LEVEL == 1
+            align_step = 4;
+#endif
+
+            if (total_elements < MULTI_THREAD_BASELINE)
+                number_of_thread = 1;
+
+            size_t chunk_size = total_elements / number_of_thread;
+            chunk_size &= ~(align_step - 1);
+
+            auto worker = [](float* src, float* tgt, size_t start, size_t end) {
+#if SIMD_LEVEL == 4
+                size_t i = start;
+                size_t limit = end - ((end - start) & ~15ULL);
+                for (; i < limit; i += 16)
+                {
+                    __m512 data_origin(_mm512_load_ps(src + i));
+                    __m512 data_other(_mm512_load_ps(tgt + i));
+                    _mm512_store_ps(src + i,
+                                    _mm512_mul_ps(data_origin, data_other));
+                }
+
+                for (; i < end; ++i)
+                {
+                    src[i] *= tgt[i];
+                }
+
+#elif SIMD_LEVEL == 3 || SIMD_LEVEL == 2
+                size_t i = start;
+                size_t limit = end - ((end - start) & ~7ULL);
+                for (; i < limit; i += 8)
+                {
+                    __m256 data_origin(_mm256_load_ps(src + i));
+                    __m256 data_other(_mm256_load_ps(tgt + i));
+                    _mm256_store_ps(src + i,
+                                    _mm256_mul_ps(data_origin, data_other));
+                }
+
+                for (; i < end; ++i)
+                {
+                    src[i] *= tgt[i];
+                }
+
+#elif SIMD_LEVEL == 1
+                size_t i = start;
+                size_t limit = end - ((end - start) & ~3ULL);
+                for (; i < limit; i += 4)
+                {
+                    __m128 data_origin(_mm_load_ps(src + i));
+                    __m128 data_other(_mm_load_ps(tgt + i));
+                    _mm_store_ps(src + i, _mm_mul_ps(data_origin, data_other));
+                }
+
+                for (; i < end; ++i)
+                {
+                    src[i] *= tgt[i];
+                }
+
+#elif SIMD_LEVEL == 0
+                for (size_t i = start; i < end; ++i)
+                    src[i] *= tgt[i];
+#endif
+            };
+
+            std::vector<std::thread> threads;
+            if (number_of_thread <= 1)
+            {
+                worker(this_cpu_ptr, other_cpu_ptr, 0, total_elements);
+                break;
+            }
+            threads.reserve(number_of_thread);
+            size_t current_start = 0;
+
+            for (size_t i = 0; i < number_of_thread - 1; ++i)
+            {
+                size_t current_end = current_start + chunk_size;
+                if (current_end > total_elements)
+                    current_end = total_elements;
+                threads.emplace_back(worker, this_cpu_ptr, other_cpu_ptr,
+                                     current_start, current_end);
+                current_start = current_end;
+            }
+
+            threads.emplace_back(worker, this_cpu_ptr, other_cpu_ptr,
+                                 current_start, total_elements);
+            for (auto& t : threads)
+                t.join();
+
             break;
         }
         case DType::FP16: {
