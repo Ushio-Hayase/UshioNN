83ac5fac2d12c96bf9beb1add729936d5c31d413
Ushio-Hayase <ushiohayase@gmail.com>
2026-03-29 14:31:44 +0900

    asdf

--- src/kernel/cpu/matmul_cpu.cpp
+++ src/kernel/cpu/matmul_cpu.cpp
@@ -19,11 +19,21 @@ void matmul_kernel(Tensor& result, const Tensor& a, const Tensor& b)
     const uint32_t a_dim = a_contiguous.dim();
     const uint32_t b_dim = b_contiguous.dim();
 
-    const auto& shape = result_contiguous.shape();
+    const auto& a_shape = a_contiguous.shape();
+    const auto& b_shape = b_contiguous.shape();
+    const auto& result_shape = result_contiguous.shape();
 
     switch (type)
     {
     case DType::FP64: {
+        ei double* result_data = result_contiguous.data_ptr<double>();
+        double* a_data = a_contiguous.data_ptr<double>();
+        double* b_data = b_contiguous.data_ptr<double>();
+
+        for (uint32_t i = 0; i < a_dim; ++i)
+            for (uint64_t j = 0; j < a_shape[i]; ++j)
+                for (uint64_t k = 0; k < b_shape[b_dim - 1]; ++k)
+                    result_data[j]
     }
     }
 }
